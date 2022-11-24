c73ec49efbcc568ceffc87a03fe9e597c6306132
Santhoshpsps <santhoshpsp4@gmail.com>
2022-11-24 20:18:46 +0530

    useEffect

---
+++ useEffect/src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(()=>{
+    const Logged_in_cookie = localStorage.getItem('Logged_in');
+    if(Logged_in_cookie === '1'){
+      setIsLoggedIn(true);
+    }
+  },[isLoggedIn])
+  
+
+  
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('Logged_in','1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
