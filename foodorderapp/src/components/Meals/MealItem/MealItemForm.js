573d43b03df288fb44c0a8a39665e3aadb6827c6
Santhoshpsps <santhoshpsp4@gmail.com>
2022-11-30 18:54:48 +0530

    Adding Cart Component & Use of Modal Wrapper

--- foodorderapp/src/components/Meals/MealItem/MealItemForm.js
+++ foodorderapp/src/components/Meals/MealItem/MealItemForm.js
@@ -5,7 +5,8 @@ const MealItemForm = (props) => {
   return (
     <form className={classes.form}>
     <Input label='Amount' input={{
-        id:'amount',
+        id: 'amount_' + props.id,
+        type:'number',
         min:'1',
         max:'5',
         step:'1',
