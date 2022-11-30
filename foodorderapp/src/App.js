ffc969034afd184d4746d07d6c7db7223757adb3
Santhoshpsps <santhoshpsp4@gmail.com>
2022-11-30 22:36:17 +0530

    Managing Cart & Modal state

--- foodorderapp/src/App.js
+++ foodorderapp/src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown,setCartIsShown]=useState(false);
+
+  const showCart=(props)=>{
+    setCartIsShown(true);
+  }
+
+  const hideCart=(props)=>{
+    setCartIsShown(false);
+  }
+
   return (
     <React.Fragment>
-    <Cart></Cart>
-      <Header></Header>
+    {cartIsShown && <Cart onClose={hideCart}/>}
+      <Header onShow={showCart}></Header>
       <main>
       <Meals></Meals>
       </main>
--- foodorderapp/src/components/Layout/Header.js
+++ foodorderapp/src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header =(props)=>{
         <React.Fragment>
             <header className={classes.header}>
                 <h1>Swiggy</h1>
-                <HeaderCartButton/>
+                <HeaderCartButton onClick={props.onShow}/>
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImg} alt="Meals Img"></img>
