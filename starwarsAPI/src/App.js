b12900b8a2fa8311c3e6a0a7d408fdd7df57c969
Santhoshpsps <santhoshpsp4@gmail.com>
2022-12-04 08:46:20 +0530

    POST - firebase

--- starwarsAPI/src/App.js
+++ starwarsAPI/src/App.js
@@ -7,13 +7,11 @@ import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
-   async function fetchMoviesHandler(){
-    
-  const response= await fetch("https://swapi.py4e.com/api/films");
-  // const response = await fetch('https://react-demo-4c8e2-default-rtdb.firebaseio.com');
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.py4e.com/api/films");
+    // const response = await fetch('https://react-demo-4c8e2-default-rtdb.firebaseio.com');
 
-  const data =await response.json();
-  
+    const data = await response.json();
 
     const transformedData = data.results.map((movie) => {
       return {
@@ -22,17 +20,30 @@ function App() {
         openingText: movie.opening_crawl,
         releaseDate: movie.release_date,
       };
-      
     });
     setMovies(transformedData);
-
   }
+
+  const onAddMovieHandler = async (movie) => {
+    const response = await fetch(
+      "https://react-demo-4c8e2-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <React.Fragment>
-    <section>
-      <AddMovie/>
-    </section>
+      <section>
+        <AddMovie onAddMovie={onAddMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
