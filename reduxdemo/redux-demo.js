ed3138c4516043b36c074827b8b8eecab8c83093
Santhoshpsps <santhoshpsp4@gmail.com>
2022-12-21 08:06:47 +0530

    more redux basics

--- reduxdemo/redux-demo.js
+++ reduxdemo/redux-demo.js
@@ -1,19 +1,29 @@
-const redux = require('redux');
+const redux = require("redux");
 
-const reducerFn = (state={counter:0},action)=>{
-    return {
-        counter: state.counter+1
-    }
-}
+const reducerFn = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+};
 
 const store = redux.legacy_createStore(reducerFn);
-console.log(store.getState())
+console.log(store.getState());
 
-const counterSubscriber=()=>{
-    const latestState = store.getState();
-    console.log(latestState);
-}
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
 
 store.subscribe(counterSubscriber);
 
-store.dispatch({type:'increment'});
+store.dispatch({ type: "decrement" });
+store.dispatch({ type: "increment" });
